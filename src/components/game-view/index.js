8c3c87efa6bf41aeb0b09b575538dbe0bfa843d2
Adrizen <tonylorenzetti2012@gmail.com>
2024-11-03 18:14:52 -0300

    Add pause & play button

--- src/components/game-view/index.js
+++ src/components/game-view/index.js
@@ -1,83 +1,138 @@
-import React, { PureComponent } from 'react'
-import { View, StatusBar, StyleSheet, SafeAreaView, Platform } from 'react-native'
-import Controls from '../controls'
-import AliensGrid from '../aliens-grid'
-import PlayerRocket from '../rocket/player-rocket'
-import AlienRocket from '../rocket/alien-rocket'
-import Sky from '../sky'
-import Explosion from '../explosion'
-import UpperBar from '../upper-bar'
+import React, { PureComponent } from "react";
+import {
+  View,
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import Controls from "../controls";
+import AliensGrid from "../aliens-grid";
+import PlayerRocket from "../rocket/player-rocket";
+import AlienRocket from "../rocket/alien-rocket";
+import Sky from "../sky";
+import Explosion from "../explosion";
+import UpperBar from "../upper-bar";
 
 export default class GameView extends PureComponent {
+  renderRockets() {
+    const {
+      rockets,
+      aliens,
+      height,
+      removeAlien,
+      removeRocket,
+      updateScore,
+      playerXPosition,
+      updateLives,
+    } = this.props;
 
-    renderRockets() {
-        const { rockets, aliens, height, removeAlien, removeRocket, updateScore, playerXPosition, updateLives } = this.props
+    return rockets.map((el) =>
+      el.player === 1 ? (
+        <PlayerRocket
+          key={el.id}
+          aliens={aliens}
+          limit={height}
+          rocketData={el}
+          removeRocket={removeRocket}
+          updateScore={updateScore}
+          removeAlien={removeAlien}
+        />
+      ) : (
+        <AlienRocket
+          key={el.id}
+          playerXPosition={playerXPosition}
+          limit={height}
+          rocketData={el}
+          removeRocket={removeRocket}
+          updateLives={updateLives}
+        />
+      )
+    );
+  }
 
-        return rockets.map(el => (
-            el.player === 1 ?
-                <PlayerRocket key={el.id} aliens={aliens} limit={height} rocketData={el} removeRocket={removeRocket} updateScore={updateScore} removeAlien={removeAlien} />
-                :
-                <AlienRocket key={el.id} playerXPosition={playerXPosition} limit={height} rocketData={el} removeRocket={removeRocket} updateLives={updateLives} />
-        ))
-    }
+  render() {
+    const {
+      score,
+      highest,
+      aliens,
+      fire,
+      width,
+      height,
+      explosion,
+      clearExplosion,
+      updatePlayerPosition,
+      lives,
+      winner,
+      exit,
+      isPaused,
+      onPausePress,
+    } = this.props;
 
-    render() {
-        const {
-            score,
-            highest,
-            aliens,
-            fire,
-            width,
-            height,
-            explosion,
-            clearExplosion,
-            updatePlayerPosition,
-            lives,
-            winner,
-            exit
-        } = this.props
+    return (
+      <SafeAreaView style={styles.base}>
+        <StatusBar barStyle="light-content" />
 
-        return (
-            <SafeAreaView style={styles.base}>
+        <View style={styles.container}>
+          <UpperBar
+            score={score}
+            highest={highest}
+            lives={lives}
+            onButtonPress={exit}
+          />
+          
+          <TouchableOpacity
+            //style={style.pauseButton}
+            onPress={onPausePress}
+          >
+            {isPaused ? <Text>Play</Text> : <Text>Pause</Text>}
+          </TouchableOpacity>
 
-                <StatusBar barStyle='light-content' />
+          {isPaused && ( <View> <Text style={{fontSize: 32}} >PAUSADO PADREEEEE</Text> </View> ) }
 
-                <View style={styles.container}>
+          <AliensGrid config={aliens} width={width} height={height} />
 
-                    <UpperBar score={score} highest={highest} lives={lives} onButtonPress={exit}/>
+          {explosion.length > 0 && (
+            <Explosion
+              variant={explosion[1] === 0 ? "2" : "1"} // Solo il cannone ha y = 0
+              position={explosion}
+              onAnimationEnd={clearExplosion}
+            />
+          )}
 
-                    <AliensGrid config={aliens} width={width} height={height} />
+          {this.renderRockets()}
 
-                    {explosion.length > 0 &&
-                        <Explosion
-                            variant={explosion[1] === 0 ? '2' : '1'} // Solo il cannone ha y = 0
-                            position={explosion}
-                            onAnimationEnd={clearExplosion}
-                        />
-                    }
+          {winner !== 2 && (
+            <Controls
+              fire={fire}
+              width={width}
+              height={height}
+              updatePlayerPosition={updatePlayerPosition}
+              lives={lives}
+            />
+          )}
+        </View>
 
-                    {this.renderRockets()}
-
-                    {winner !== 2 && <Controls fire={fire} width={width} height={height} updatePlayerPosition={updatePlayerPosition} lives={lives}/>}
-                    
-                </View>
-
-                <Sky width={width} height={height} />
-
-            </SafeAreaView>
-        )
-    }
+        <Sky width={width} height={height} />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    base: {
-        backgroundColor: '#000',
-        flex: 1
-    },
-    container: {
-        //backgroundColor: 'orangered',
-        paddingTop: Platform.OS === 'ios' ? 4 : 24,
-        flex: 1,
-        zIndex: 1
-    }
-})
+  base: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
+  container: {
+    //backgroundColor: 'orangered',
+    paddingTop: Platform.OS === "ios" ? 4 : 24,
+    flex: 1,
+    zIndex: 1,
+  },
+  pauseButton: {
+    position: 'absolute',
+  }
+});
